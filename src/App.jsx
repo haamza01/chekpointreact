f839021589a51ca9f31ead88a543834718e87fe1
hamzza01 <hza6766@gmail.com>
2021-12-08 10:29:03 +0100

    checkpoint

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './navbar.jsx'
+import Checkpoint from './checkpoint.jsx'
+
+function App() {
+    return ( <
+        div className = "App" >
+         <Navbar / >
+         <Checkpoint />
+        </div>
+
+       
+    );
+}
+
+export default App;
---
+++ src/navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar  ()   {
+
+
+
+    return (
+
+
+        <ul>
+        <li > home </li>
+         <li> about </li> 
+         </ul>
+    )
+
+
+
+}
+export default Navbar
